24c10818506dd9c4ae292c7b010a5d07fd073fc4
sss2928-netizen <ss2928@srmist.edu.in>
2026-02-24 08:55:06 +0530

    Coded UC2 to Render OOPS as Banner using Print Statements

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,13 +1,33 @@
 /**
  * OOPSBannerApp
- * UC1: Print OOPS to Console
+ * UC2: Render OOPS as Banner using Print Statements
  * 
  * @author YourName
- * @version 1.0
+ * @version 2.0
  */
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
-        System.out.println("OOPS");
+
+        // Line 1
+        System.out.println("  *****     *****     *******    ******* ");
+        
+        // Line 2
+        System.out.println(" *     *   *     *    *      *   *       ");
+        
+        // Line 3
+        System.out.println("*       * *       *   *      *   *       ");
+        
+        // Line 4
+        System.out.println("*       * *       *   *******    ******  ");
+        
+        // Line 5
+        System.out.println("*       * *       *   *          *       ");
+        
+        // Line 6
+        System.out.println(" *     *   *     *    *          *       ");
+        
+        // Line 7
+        System.out.println("  *****     *****     *          ******* ");
     }
 }
